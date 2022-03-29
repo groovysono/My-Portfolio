f5c4dba633a6e8b47e4766a444ca3ae7ac62e338
Arinze Onwubuya <81771611+groovysono@users.noreply.github.com>
2022-03-28 19:46:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ var typed = new Typed(".typing", {
   backSpeed: 60,
   loop: true
 });
+
+$(document).ready(function(){
+  $('nav .navbar li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $(' nav .navbar').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+});
